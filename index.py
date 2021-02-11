759fe780b339d74dd0c4374885dc825283a0c05d
Glenn <gjgalvizo@gmail.com>
2021-02-10 17:16:32 -0800

    Adding some structure to make this program a bit executable. We can specify any config-related information in config.json, and it should should be passed around by each class.

--- index.py
+++ index.py
@@ -1,3 +1,5 @@
+import argparse
+import json
 
 
 class Tokenizer:
@@ -19,13 +21,25 @@ class StorageHandler:
 
 
 class Indexer:
-    def index(self, path):
-        for file in load(path):
+    def __init__(self, corpus, **kwargs):
+        self.corpus = corpus
+        self.config = kwargs
+        pass
+
+    def index(self):
+        for file in load(self.corpus):
             pass
     pass
 
 
 
 if __name__ == '__main__':
-    # TODO
-    pass
+    parser = argparse.ArgumentParser(description='Build an inverted index given some corpus.')
+    parser.add_argument('corpus', type=str, help='Location of the corpus to index.')
+    parser.add_argument('--config', type=str, default='config.json', help='Path to the config file.')
+    command_line_args = parser.parse_args()
+    with open(command_line_args.config) as config_file:
+        main_config_json = json.load(config_file)
+
+    # Invoke our indexer.
+    Indexer(command_line_args.corpus, **main_config_json).index()
