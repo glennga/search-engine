799cca4fada350244c363cd1a2eff9f2bc7fddbc
Glenn <gjgalvizo@gmail.com>
2021-02-12 13:35:25 -0800

    First try at the storage handler. This approach assumes that the inverted lists themselves can fit into memory, but this might not be a valid assumption. Next work though, is adding skip pointers to decrease the search time from O(n) to O(log(n)).

--- index.py
+++ index.py
@@ -3,8 +3,16 @@ import argparse
 import json
 import sys
 import datetime
+import pickle
+import os
+import re
+import heapq
+
 from lxml import etree, html
 from pathlib import Path
+from collections import deque
+from sortedcontainers import SortedList
+
 
 logger = __init__.get_logger('Index')
 
@@ -49,19 +57,195 @@ class Tokenizer:
         return tokens
 
 
-
 class StorageHandler:
+    """ Class to manage the storage of our inverted index.
+
+    The StorageHandler class is responsible for the following:
+    1. Managing an in-memory inverted index, represented as a hash map (python dictionary).
+    2. Spilling these in-memory components to a new disk component when we pass some "spillThreshold".
+    3. Merging all partial components into a single inverted file when "close" is called.
+
+    Some notable features about this inverted index are:
+    a) Each component is *immutable*. This allows us to simplify reasoning about flushing and merging, but at the cost
+       of not being able to update values. ElasticSearch similarly adopts this practice of having immutable components.
+    b) The merging process happens "all-at-once" once "close" is called (rather than a "rolling merge" approach).
+    c) The resulting inverted index consists of a *single* file (as opposed to multiple).
+    d) A single inverted list MUST be able to fit into memory, but <token, inverted list> pairs can extend out of
+       memory. TODO: See if this is a valid assumption...
+
+    """
+    def __init__(self, **kwargs):
+        self.merge_queue = deque()
+        self.memory_component = dict()
+        self.config = kwargs
 
     def write(self, token, entry):
-        """
+        """ Store an index entry with respect to a single document.
 
-        :param token:
-        :param entry: [docID, frequency, <tuple of tags>]
-        :return:
-        """
-        pass
+        To account for large lists in memory, we use a SortedList structure which performs insertions in approximately
+        O(log(n)) time.
 
-    pass
+        :param token: The token to associate with the given entry.
+        :param entry: The document descriptor, consisting of the following: [docID, frequency, <tuple of tags>]
+        """
+        if token not in self.memory_component:
+            self.memory_component[token] = SortedList(key=lambda ell: ell[0])
+        self.memory_component[token].add(entry)
+
+        # Trigger a spill action.
+        if sys.getsizeof(self.memory_component) > self.config['storage']['spillThreshold']:
+            self._spill()
+
+    def close(self) -> str:
+        """ Merge all of our partial disk components and the current in-memory component to a single inverted file.
+
+        1. If we do not have any disk components, then our entire index resides in memory. Spill this to disk, and move
+           the result to the data directory.
+        2. Otherwise, we need to perform some merging. If the in-memory component is currently populated, then merge the
+           in-memory component and the first file in our merge queue. Add the resultant file to the queue.
+        3. Pull two files from the merge queue and merge them together. Repeat this until we only have one file in our
+           merge queue.
+        4. Move the remaining disk component file to the specified data directory. The merge is finished.
+
+        :return The name of the index file generated.
+        """
+        if len(self.merge_queue) == 0:
+            logger.info('No disk components found. Writing in-memory component to disk.')
+            self._spill()
+
+        while len(self.merge_queue) > 1:
+            merge_level = min(int(re.search(r'.*/d([0-9]).*', f).group(1)) for f in self.merge_queue) + 1
+            run_file = f'd{merge_level}_' + str(datetime.datetime.now().isoformat()) + '.comp'
+            logger.info(f'Starting merge to generate file {run_file}.')
+
+            if len(self.memory_component) > 0:
+                with open(self.merge_queue.popleft(), 'rb') as right_fp, \
+                     open(self.config['storage']['spillDirectory'] + '/' + run_file, 'wb') as out_fp:
+                    logger.info(f'Performing merge on in-memory component and {right_fp} component.')
+                    left_component = sorted(self.memory_component.items(), key=lambda token: token[0])
+                    right_component = self._deserialize_generator(right_fp)
+                    self._merge(left_component, right_component, out_fp)
+                    self.memory_component.clear()
+
+            else:
+                with open(self.merge_queue.popleft(), 'rb') as left_fp, \
+                     open(self.merge_queue.popleft(), 'rb') as right_fp, \
+                     open(self.config['storage']['spillDirectory'] + '/' + run_file, 'wb') as out_fp:
+                    logger.info(f'Performing merge on {left_fp} component and {right_fp} component.')
+                    left_component = self._deserialize_generator(left_fp)
+                    right_component = self._deserialize_generator(right_fp)
+                    self._merge(left_component, right_component, out_fp)
+
+            self.merge_queue.append(self.config['storage']['spillDirectory'] + '/' + run_file)
+
+        data_file = str(datetime.datetime.now().isoformat()) + '.idx'
+        os.rename(self.merge_queue.popleft(), self.config['storage']['dataDirectory'] + '/' + data_file)
+        logger.info(f'Merge has finished. Index {data_file} has been built.')
+        return data_file
+
+    def _spill(self):
+        """ Spill the current in-memory component to disk.
+
+        1. Give total order to memory component. We will store each <token, inverted list> in alphabetical token order.
+        2. Write each <token, inverted list> pair incrementally to the given file pointer. We are using "pickle" to
+           serialize each pair.
+
+        """
+        spill_file = 'd0_' + str(datetime.datetime.now().isoformat()) + '.comp'
+        logger.info(f'Spilling component {spill_file} to disk.')
+
+        with open(self.config['storage']['spillDirectory'] + '/' + spill_file, 'wb') as spill_fp:
+            ordered_memory_component = sorted(self.memory_component.items(), key=lambda token: token[0])
+            for disk_entry in ordered_memory_component:
+                native_entry = (disk_entry[0], list(disk_entry[1]))
+                logger.debug(f'Writing entry {native_entry} to disk.')
+                pickle.dump(native_entry, spill_fp)
+
+        self.memory_component.clear()
+        self.merge_queue.append(self.config['storage']['spillDirectory'] + '/' + spill_file)
+
+    @staticmethod
+    def _merge(left_component, right_component, out_fp):
+        """ Merge the two components together in a "merge-join" fashion, writing to the given file pointer.
+
+        1. Open a new disk component, prefixed with "d{n}", where n = the current merge depth.
+        2. Open the disk component, and get a generator to the token pairs. The in-memory component will henceforth
+           be called the "left" component and the disk component will be called the "right" component.
+        3. Maintain two "logical" cursors: a left cursor and right cursor. Begin the merging process.
+           a) If the left cursor token equals the right cursor token, merge the two lists and write the result to disk.
+              Both inverted lists are already sorted, so we can perform a similar "merge" approach at the inverted list
+              granularity. Advance both cursors.
+           b) If the left cursor token is greater than the right cursor token, then write the right token pair to disk.
+              Advance only the right cursor.
+           c) If the left cursor token is less than the right cursor token, then write the left token pair to disk.
+              Advance only the left cursor.
+           d) If we have exhausted any of the two lists, then write the remainder of the to-be-exhausted list to disk.
+
+        :param left_component: Iterator that produces sorted <token, inverted list> pairs.
+        :param right_component: Iterator that produces sorted <token, inverted list> pairs.
+        """
+        def _advance(component):
+            try:
+                token_pair = next(component)
+                is_exhausted = False
+            except StopIteration:
+                token_pair = None
+                is_exhausted = True
+            return token_pair, is_exhausted
+
+        left_token_pair, is_left_exhausted = _advance(left_component)
+        right_token_pair, is_right_exhausted = _advance(right_component)
+        while True:
+            if not is_left_exhausted and not is_right_exhausted:
+                if left_token_pair[0] == right_token_pair[0]:
+                    out_entry = (left_token_pair[0], heapq.merge(left_token_pair[1], right_token_pair[1]))
+                    left_token_pair, is_left_exhausted = _advance(left_component)
+                    right_token_pair, is_right_exhausted = _advance(right_component)
+
+                elif left_token_pair[0] > right_token_pair[0]:
+                    out_entry = right_token_pair
+                    right_token_pair, is_right_exhausted = _advance(right_component)
+
+                else:  # left_token_pair[0] < right_token_pair[0]
+                    out_entry = left_token_pair
+                    left_token_pair, is_left_exhausted = _advance(left_component)
+
+            elif is_left_exhausted and not is_right_exhausted:
+                out_entry = right_token_pair
+                right_token_pair, is_right_exhausted = _advance(right_component)
+
+            elif not is_left_exhausted and is_right_exhausted:
+                out_entry = left_token_pair
+                left_token_pair, is_left_exhausted = _advance(left_component)
+
+            else:  # is_left_exhausted and is_right_exhausted
+                break
+
+            logger.debug(f'Writing entry {out_entry} to disk.')
+            pickle.dump(out_entry, out_fp)
+
+    @staticmethod
+    def _deserialize_generator(in_fp):
+        """ Deserialize the given ordered disk component to the an ordered list of <token, inverted list> pairs.
+
+        To avoid having to load the entire disk component into memory, we return a generator of <token, inverted list>
+        pairs. The order in which the generator returns these pairs is the same order in which it was serialized (in
+        alphabetical order of the tokens).
+
+        :param in_fp: Binary file pointer of the disk component to deserialize.
+        :return: A generator of <token, inverted list> pairs.
+        """
+        def _token_pair_generator():
+            try:
+                while True:
+                    token_pair = pickle.load(in_fp)
+                    logger.debug(f'Returning entry {token_pair} to caller.')
+                    yield token_pair
+
+            except EOFError:
+                return
+
+        return _token_pair_generator()
 
 
 class Indexer:
