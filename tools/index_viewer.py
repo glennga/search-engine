47a8eab3543e6f802b9307657e85478b9b660596
Glenn <gjgalvizo@gmail.com>
2021-02-15 12:36:45 -0800

    Updating index_viewer to now search for words using the index. On my super small word set, it works so far!

--- tools/index_viewer.py
+++ tools/index_viewer.py
@@ -1,8 +1,11 @@
 import argparse
 import pickle
 
+# Required for pickle!
+from index import IndexDescriptor
 
-def _deserialize_generator(in_fp):
+
+def _generator(in_fp):
     """ Deserialize the given ordered disk component to the an ordered list of <token, inverted list> pairs.
 
     To avoid having to load the entire disk component into memory, we return a generator of <token, inverted list>
@@ -12,7 +15,6 @@ def _deserialize_generator(in_fp):
     :param in_fp: Binary file pointer of the disk component to deserialize.
     :return: A generator of <token, inverted list> pairs.
     """
-
     def _token_pair_generator():
         try:
             while True:
@@ -24,12 +26,41 @@ def _deserialize_generator(in_fp):
     return _token_pair_generator()
 
 
+def _search(in_fp, in_desc, word):
+    """ Search for a given word using the index descriptor and the index file. """
+    designated_tell = in_desc[word]
+    if designated_tell is None:
+        print(f'Could not find larger entry in index, starting from position {0}')
+        designated_tell = 0
+    else:
+        print(f'Starting from position {designated_tell}.')
+
+    in_fp.seek(designated_tell)
+    search_generator = _generator(in_fp)
+    for data_entry in search_generator:
+        if data_entry[0] == word:
+            return data_entry
+
+        elif data_entry[0] > word:
+            print('Word does not exist!')
+            return None
+
+    print('Word does not exist!')
+    return None
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='View the contents of an index file, in sequential order.')
     parser.add_argument('index', type=str, help='Location of the index file to read.')
+    parser.add_argument('desc', type=str, help='Location of the descriptor file to read.')
+    parser.add_argument('--word', type=str, default=None, help='Word to search using the index.')
     command_line_args = parser.parse_args()
 
-    with open(command_line_args.index, 'rb') as index_fp:
-        generator = _deserialize_generator(index_fp)
-        for entry in generator:
-            print(entry)
+    with open(command_line_args.index, 'rb') as index_fp, open(command_line_args.desc, 'rb') as desc_fp:
+        if command_line_args.word is None:
+            generator = _generator(index_fp)
+            for entry in generator:
+                print(entry)
+        else:
+            descriptor = next(_generator(desc_fp))
+            print(_search(index_fp, descriptor, command_line_args.word))
